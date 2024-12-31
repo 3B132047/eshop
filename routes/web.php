3b1b9185730440bbc5f387354cf8c23749fb5224
hello-world <7iuy6u@gmail.com>
2024-12-31 20:55:28 +0800

    初始專案

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use app\Http\Middleware\EnsureTokenIsValid;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/test', function () {
+    return 'pass';
+   }) ->middleware(EnsureTokenIsValid::class);
